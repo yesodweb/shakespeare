62a1b15898731f160e1c5f3c5f15f992c258a224
Greg Weber <greg@gregweber.info>
2011-08-06 09:15:16 -0700

    add Setup.lhs to all projects

--- install-all.sh
+++ install-all.sh
@@ -2,13 +2,7 @@
 
 CABAL=cabal
 
-# for Yesod we don't need xml-hamlet
-# cd hamlet
-#($CABAL configure --enable-tests ||
-    #($CABAL install --only-dependencies && $CABAL configure --enable-tests)
-#) && $CABAL build && $CABAL test && ./Setup.lhs install || exit
-
-PACKAGES="hamlet xml-hamlet"
+PACKAGES="shakespeare xml-hamlet shakespeare-css shakespeare-js hamlet"
 for package in $PACKAGES
 do
     echo Installing $package
--- xml-hamlet/Setup.lhs
+++ xml-hamlet/Setup.lhs
@@ -2,6 +2,7 @@
 
 > module Main where
 > import Distribution.Simple
+> import System.Cmd (system)
 
 > main :: IO ()
 > main = defaultMain
