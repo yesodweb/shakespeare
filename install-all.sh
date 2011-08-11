a6e5ef3f1e849603cc1864c49cb4a953fc0a696f
Greg Weber <greg@gregweber.info>
2011-08-11 09:44:26 -0700

    add testing dependencies to ./install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -8,7 +8,7 @@ do
     echo Installing $package
     cd $package
     ($CABAL configure --enable-tests ||
-      ($CABAL update && $CABAL install --only-dependencies && $CABAL configure --enable-tests)
+      ($CABAL update && $CABAL install HUnit QuickCheck hspec && $CABAL install --only-dependencies && $CABAL configure --enable-tests)
     ) && $CABAL build && $CABAL test && ./Setup.lhs install || exit 1
     cd ..
 done
