ea192b12cc6cfbf2ee2fa24c5058a521590258f9
Greg Weber <greg@gregweber.info>
2011-08-11 08:00:42 -0700

    ./install-all.sh - work from scratch

--- install-all.sh
+++ install-all.sh
@@ -2,13 +2,13 @@
 
 CABAL=cabal
 
-PACKAGES="shakespeare xml-hamlet shakespeare-css shakespeare-js hamlet"
+PACKAGES="shakespeare shakespeare-css shakespeare-js hamlet xml-hamlet"
 for package in $PACKAGES
 do
     echo Installing $package
     cd $package
     ($CABAL configure --enable-tests ||
-      ($CABAL install --only-dependencies && $CABAL configure --enable-tests)
+      ($CABAL update && $CABAL install --only-dependencies && $CABAL configure --enable-tests)
     ) && $CABAL build && $CABAL test && ./Setup.lhs install || exit 1
     cd ..
 done
