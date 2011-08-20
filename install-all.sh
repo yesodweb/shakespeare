f7bfc7babdf5023dcbaa0324e0012ed83f932b31
Greg Weber <greg@gregweber.info>
2011-08-20 10:03:59 -0700

    tighten hspec version

--- install-all.sh
+++ install-all.sh
@@ -3,7 +3,7 @@
 CABAL=cabal
 
 # install testing dependencies
-$CABAL install HUnit QuickCheck hspec && 
+$CABAL install HUnit QuickCheck 'hspec >= 0.6.1 && < 0.7' || exit 1
 
 PACKAGES="shakespeare shakespeare-css shakespeare-js shakespeare-text hamlet xml-hamlet"
 for package in $PACKAGES
@@ -11,7 +11,7 @@ do
     echo Installing $package
     cd $package
     ($CABAL configure --enable-tests ||
-      ($CABAL install --only-dependencies && $CABAL configure --enable-tests)
+        ($CABAL install --only-dependencies && $CABAL configure --enable-tests)
     ) && $CABAL build && $CABAL test && ./Setup.lhs install || exit 1
     cd ..
 done
