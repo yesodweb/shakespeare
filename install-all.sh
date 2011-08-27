518fc32bb66626a6312a8c04836656cb7e5034c6
patrick brisbin <pbrisbin@gmail.com>
2011-08-27 10:29:11 -0400

    install-all.sh: small rewrite

--- install-all.sh
+++ install-all.sh
@@ -1,17 +1,33 @@
-#!/bin/sh
+#!/bin/bash -e
 
-CABAL=cabal
+# allow a CABAL env var to override
+CABAL=${CABAL:-cabal}
 
 # install testing dependencies
-$CABAL install HUnit QuickCheck 'hspec >= 0.6.1 && < 0.7' || exit 1
+$CABAL install HUnit QuickCheck 'hspec >= 0.6.1 && < 0.7'
 
-PACKAGES="shakespeare shakespeare-css shakespeare-js shakespeare-text hamlet xml-hamlet"
-for package in $PACKAGES
-do
-    echo Installing $package
-    cd $package
-    ($CABAL configure --enable-tests ||
-        ($CABAL install --only-dependencies && $CABAL configure --enable-tests)
-    ) && $CABAL build && $CABAL test && ./Setup.lhs install || exit 1
-    cd ..
+pkgs=( shakespeare
+       shakespeare-css
+       shakespeare-js
+       shakespeare-text
+       hamlet
+       xml-hamlet
+     )
+
+# install each sub-respository
+for pkg in "${pkgs[@]}"; do
+  echo "Installing $pkg..."
+
+  (
+    cd "./$pkg"
+
+    if ! $CABAL configure --enable-tests; then
+      $CABAL install --only-dependencies
+      $CABAL configure --enable-tests
+    fi
+
+    $CABAL build
+    $CABAL test
+    ./Setup.lhs install
+  )
 done
