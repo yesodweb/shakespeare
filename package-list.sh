037115f1dbb73d74614e04b8a03e48648c26b1b5
Michael Snoyman <michael@snoyman.com>
2011-12-14 09:26:29 +0200

    package-list.sh

---
+++ package-list.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pkgs=(     ./shakespeare
+           ./shakespeare-css
+           ./shakespeare-js
+           ./shakespeare-text
+           ./shakespeare-i18n
+           ./hamlet                )
