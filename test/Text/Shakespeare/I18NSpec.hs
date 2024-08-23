c094ed9774b3fd41cb3ca80d4a694ec53cdb7b03
Oleksandr-Zhabenko <oleksandr.zhabenko@yahoo.com>
2024-08-23 16:23:08 +0300

    Added support in mkMessage for context parsing #282
    Added support for lts versions 22, 21, 20 and removed older ones
    
    Added tests for double mkMessage in I18NSpec.hs

--- test/Text/Shakespeare/I18NSpec.hs
+++ test/Text/Shakespeare/I18NSpec.hs
@@ -1,16 +1,37 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+
 module Text.Shakespeare.I18NSpec
     ( spec
     ) where
 
 import           Data.Text             (Text)
 import           Text.Shakespeare.I18N
+import           Language.Haskell.TH.Syntax
+import           Test.Hspec
+
+class Lift master => YesodSubApp master where
+   data YesodSubAppData master :: *
+
+newtype SubApp master = SubApp
+  {
+     getOrdering :: Ordering
+  }
+
+data Test = Test
+
+class Testable a where
+  isTestable :: a -> Bool 
+
+instance Testable Test where 
+  isTestable Test = True
 
 spec :: Monad m => m ()
 spec = return ()
 
-data Test = Test
+mkMessage "(YesodSubApp master) => SubApp master" "other-messages" "en" 
 
 mkMessage "Test" "test-messages" "en"
+
