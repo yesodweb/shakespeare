665af8b72492b025417f7d845e8279b677450d7c
Benjamin McRae <benjamin@supercede.com>
2025-07-01 15:25:33 +0100

    some hlint/ghc complaints

--- test/Text/Shakespeare/I18NSpec.hs
+++ test/Text/Shakespeare/I18NSpec.hs
@@ -10,23 +10,12 @@ module Text.Shakespeare.I18NSpec
 import           Data.Text             (Text)
 import           Text.Shakespeare.I18N
 import           Language.Haskell.TH.Syntax
-import           Test.Hspec
 
 class Lift master => YesodSubApp master where
-   data YesodSubAppData master :: *
 
-newtype SubApp master = SubApp
-  {
-     getOrdering :: Ordering
-  }
+data SubApp master
 
-data Test a b = Test
-
-class Testable a where
-  isTestable :: a -> Bool 
-
-instance Testable Test where 
-  isTestable Test = True
+data Test a b
 
 spec :: Monad m => m ()
 spec = return ()
@@ -34,4 +23,3 @@ spec = return ()
 mkMessage "(YesodSubApp master) => SubApp master" "other-messages" "en" 
 
 mkMessage "Test a b" "test-messages" "en"
-
